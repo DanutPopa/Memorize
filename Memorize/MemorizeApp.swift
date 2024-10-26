6fc889c7d392023130947d99bdb9b715e65fd413
DanutPopa <popa.ion.danut@gmail.com>
2024-10-26 16:12:07 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Danut Popa on 26.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

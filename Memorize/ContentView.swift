0b31a63e1d732ed6979abda72c7c347670312b74
DanutPopa <popa.ion.danut@gmail.com>
2025-05-15 15:50:22 +0300

    Finished Programming Assigment 1.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,62 +8,98 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "🦊", "😱", "☠️", "🍭"]
-    
-    @State var cardCount = 4
+    @State var emojis: [String] = []
+    @State var colorTheme = Color.clear
     
     var body: some View {
         VStack {
+            Text("Memorize!")
+                .font(.largeTitle)
+                .fontWeight(.medium)
+            
             ScrollView {
                 cards
             }
             Spacer()
-            cardCountAdjusters
+            HStack(alignment: .lastTextBaseline, spacing: 50) {
+                vehiclesButtonTheme
+                animalsButtonTheme
+                natureButtonTheme
+            }
         }
         .padding()
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: widthThatBestFits(cardCount: emojis.count)))]) {
+            ForEach(emojis.indices, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
-      .foregroundStyle(.orange)
+      .foregroundStyle(colorTheme)
+    }
+        
+    var vehiclesButtonTheme: some View {
+        changeTheme(with: "Vehicles", label: "car", color: .blue, contents: Constants.vehicles)
     }
     
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
+    var animalsButtonTheme: some View {
+        changeTheme(with: "Animals", label: "dog", color: .red, contents: Constants.animals)
+    }
+    
+    var natureButtonTheme: some View {
+        changeTheme(with: "Nature", label: "leaf", color: .green, contents: Constants.nature)
+    }
+    
+    func changeTheme(with name: String, label: String, color: Color, contents: [String]) -> some View {
+        Button(action: {
+            updateEmojis(with: contents)
+            colorTheme = color
+        }, label: {
+            VStack {
+                Image(systemName: label)
+                    .imageScale(.large)
+                    .font(.title)
+                
+                Text(name)
+                    .font(.subheadline)
+            }
+        })
+    }
+    
+    func updateEmojis(with contents: [String]) {
+        if emojis.isEmpty == false {
+            emojis.removeAll()
         }
-        .imageScale(.large)
-        .font(.largeTitle)
+        let upperLimit = Int.random(in: 3..<contents.count)
+        emojis.append(contentsOf: contents[0...upperLimit])
+        emojis = emojis.shuffled()
     }
     
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button {
-            cardCount += offset
-        } label: {
-            Image(systemName: symbol)
+    func widthThatBestFits(cardCount: Int) -> CGFloat {
+//        var multiplier = 10
+//        if cardCount <= 5 {
+//            multiplier = 20
+//        } else if cardCount >= 10 {
+//            multiplier = 5
+//        }
+//        return CGFloat(cardCount * multiplier)
+        let multiplier = switch cardCount {
+        case 1...5: 20.0
+        case 6..<10: 10.0
+        case 10...16: 5.0
+        case 17...21: 3.0
+        case 22...25: 2.5
+        default: 1.0
         }
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    
-    var cardRemover: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-    
-    var cardAdder: some View {
-        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+        return CGFloat(Double(cardCount) * multiplier)
     }
 }
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp = true
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
